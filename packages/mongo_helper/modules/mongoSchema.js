ba947ae717e6b7cde55318c6a9837a1f991d3e9a
ChangChao-Tang <mrgmp2004@hotmail.com>
2017-12-03 12:12:07 +0800

    add find and aggregate

--- packages/mongo_helper/modules/mongoSchema.js
+++ packages/mongo_helper/modules/mongoSchema.js
@@ -1,3 +1,4 @@
+const qs = require('qs')
 exports.name = 'MongoSchema'
 exports.version = '0.0.1'
 
@@ -37,7 +38,7 @@ function createCursor (collection, query, option) {
 }
 
 /**
- * @description The helper function will use insertOne to create record
+ * @description The helper function will use `insertOne` to create record
  * @param {object} collection a mongodb collection
  * @param {function} dataBuilder a function return update object, the parameters are [error, model, helper]
  * @param {function} optionBuilder a function return option object, the parameters are [error, model, helper]
@@ -67,7 +68,7 @@ function create(collection, dataBuilder, optionBuilder, responseDelegate) {
 exports.schemaCreate = create
 
 /**
- * @description The helper function will use findOneAndUpdate to save data
+ * @description The helper function will use `findOneAndUpdate` to save data
  * @param {object} collection a mongodb collection
  * @param {function} queryBuilder a function return query object, the parameters are [error, model, helper]
  * @param {function} dataBuilder a function return update object, the parameters are [error, model, helper]
@@ -99,7 +100,7 @@ function save(collection, queryBuilder, dataBuilder, optionBuilder, responseDele
 exports.schemaSave = save
 
 /**
- * @description The helper function will use findOne to get one record
+ * @description The helper function will use `findOne` to get one record
  * @param {object} collection a mongodb collection
  * @param {function} queryBuilder a function return query object, the parameters are [error, model, helper]
  * @param {function} optionBuilder a function return option object, the parameters are [error, model, helper]
@@ -107,7 +108,7 @@ exports.schemaSave = save
  * */
 
 function getOne (collection, queryBuilder, optionBuilder, responseDelegate) {
-  return function (error, model, helper, cb, ctrl) {
+  return function get_schema_delegate(error, model, helper, cb, ctrl) {
     const query = queryBuilder(error, Object.assign({}, model.$clean()), helper),
       option = optionBuilder(error, Object.assign({}, model.$clean()), helper)
     if (error.hasError('')){
@@ -125,6 +126,137 @@ function getOne (collection, queryBuilder, optionBuilder, responseDelegate) {
 }
 
 exports.schemaGet = getOne
+
+/**
+ * @description The function will use `find` to get one record, please noted the controller must pass helper with structure like:
+ * {
+ *  sort,
+ *  page,
+ *  per_page,
+ *  project
+ *  .
+ *  .
+ *  .
+ *  other query fields
+ * }
+ * @param {object} collection a mongodb collection
+ * @param {function} queryBuilder a function return query object, the parameters are [error, helper]
+ * @param {string} baseURL the api baseURL, used to generate page information
+ * @param {function} responseDelegate a function send response, the parameters are [error, result, controller]
+ * */
+
+function queryByFind (collection, queryBuilder, baseURL, responseDelegate) {
+  return function find_schema_delegate(error, helper, callback, ctrl) {
+    if (!helper){
+      error.push('query error', 'no query helper')
+      return responseDelegate(error, undefined, ctrl)
+    }
+    helper.page = parseInt(helper.page) || 1
+    const per_page = parseInt(helper.per_page) || 10
+    helper.per_page = per_page > 100 ? 100 : per_page
+
+    const pagination = {},
+      q = Object.assign({}, helper)
+    delete q.sort
+    delete q.page
+    delete q.per_page
+    delete q.project
+    const query = queryBuilder(error, q)
+
+    if (error.hasError('')){return responseDelegate(error, undefined, ctrl)}
+
+    const c = {}
+    c.sort = helper.sort
+    c.project = helper.project
+    c.skip = (helper.page - 1) * helper.per_page
+    c.limit = helper.per_page
+    const option = cursorOption(c.sort, c.project, c.skip, c.limit)
+    const cursor = createCursor(collection, query, option)
+
+    cursor.toArray()
+      .then((docs)=>{
+        const next_helper = Object.assign({}, helper)
+        next_helper.page += 1
+        pagination.next_page_url = docs.length < helper.per_page ? '' :
+          baseURL + ctrl.url + '?' + qs.stringify(next_helper)
+        const prev_helper = Object.assign({}, helper)
+        prev_helper.page -= 1
+        pagination.prev_page_url = helper.page === 1 ? '' :
+          baseURL + ctrl.url + '?' + qs.stringify(prev_helper)
+        const res = {
+          links: {pagination},
+          data: docs
+        }
+        return responseDelegate(error, res, ctrl)
+      })
+      .catch((err)=>{
+        error.push('mongodb error', err.toString())
+        return responseDelegate(error, undefined, ctrl)
+      })
+  }
+}
+
+exports.schemaFind = queryByFind
+
+/**
+ * @description the function will use `aggregate` to query, the controller must pass helper with structure like:
+ * {
+ *  sort,
+ *  page,
+ *  per_page,
+ *  project
+ *  .
+ *  .
+ *  .
+ *  other query fields
+ * }
+ * @param {object} collection a mongodb collection
+ * @param {function} pipelineBuilder a function return query object, the parameters are [error, helper]
+ * @param {string} baseURL the api baseURL, used to generate page information
+ * @param {function} responseDelegate a function send response, the parameters are [error, result, controller]
+ * */
+
+function queryByAggregate (collection, pipelineBuilder, baseURL, responseDelegate) {
+  return function aggregate_schema_delegate(error, helper, cb, ctrl) {
+    if (!helper){
+      error.push('query error', 'no query helper')
+      return responseDelegate(error, undefined, ctrl)
+    }
+    helper.page = parseInt(helper.page) || 1
+    const per_page = parseInt(helper.per_page) || 10
+    helper.per_page = per_page > 100 ? 100 : per_page
+
+    const pipeline = pipelineBuilder(error, helper)
+
+    if (error.hasError('')){
+      return responseDelegate(error, undefined, ctrl)
+    }
+    
+    collection.aggregate(pipeline).toArray()
+      .then((docs)=>{
+        const pagination = {}
+        const next_helper = Object.assign({}, helper)
+        next_helper.page += 1
+        pagination.next_page_url = docs.length < helper.per_page ? '' :
+          baseURL + ctrl.url + '?' + qs.stringify(next_helper)
+        const prev_helper = Object.assign({}, helper)
+        prev_helper.page -= 1
+        pagination.prev_page_url = helper.page === 1 ? '' :
+          baseURL + ctrl.url + '?' + qs.stringify(prev_helper)
+        const res = {
+          links: {pagination},
+          data: docs
+        }
+        return responseDelegate(error, res, ctrl)
+      })
+      .catch((err)=>{
+        error.push('mongodb error', err.toString())
+        return responseDelegate(error, undefined, ctrl)
+      })
+  }
+}
+
+exports.schemaAggregate = queryByAggregate
 
 
 
