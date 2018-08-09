d593a8baf8d669dc9a651b67d78fd3fd1ddf0082
ChangChao-Tang <mrgmp2004@hotmail.com>
2018-08-09 15:05:45 +0800

    update

--- modules/JWT/JsonWebToken.js
+++ modules/JWT/JsonWebToken.js
@@ -2,18 +2,25 @@ const jwt = require('jsonwebtoken')
 const F = global.framework
 const CONFIG = global.CONFIG
 
-F.encrypt = function(value) {
-  try {
-    return jwt.sign(value, CONFIG('secret-key'))
-  } catch (e) {
-    return null
+exports.name = 'JSONWebToken'
+exports.version = '0.0.1'
+exports.booting = true
+
+exports.install = () => {
+  F.encrypt = function(value) {
+    try {
+      return jwt.sign(value, CONFIG('secret-key'))
+    } catch (e) {
+      return null
+    }
+  }
+  
+  F.decrypt = function(value) {
+    try {
+      return jwt.verify(value, CONFIG('secret-key'))
+    } catch (e) {
+      return null
+    }
   }
 }
 
-F.decrypt = function(value) {
-  try {
-    return jwt.verify(value, CONFIG('secret-key'))
-  } catch (e) {
-    return null
-  }
-}
