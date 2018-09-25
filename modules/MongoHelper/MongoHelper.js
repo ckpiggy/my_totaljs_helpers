561b49d3f2d510b30fd4bc02951e2f886c8ebc1a
ChangChao-Tang <mrgmp2004@hotmail.com>
2018-09-25 10:41:48 +0800

    connect after whole framework loaded

--- modules/MongoHelper/MongoHelper.js
+++ modules/MongoHelper/MongoHelper.js
@@ -19,15 +19,16 @@ exports.install = (options)=>{
     poolSize: 100,
     useNewUrlParser: true
   }, options)
-  const poolSize = options.poolSize
-  mongodb.MongoClient.connect(url, opt, function (error, client) {
-    if (error)
-      throw error
-    const urlObj = Url.parse(url)
-    const dbName = urlObj.pathname.replace('/', '')
-    F.MongoDB = client.db(dbName)
-    F.emit('database', F.MongoDB)
-    console.log('connected to database ' + dbName)
-    F.wait('mongodb')
+  F.on('load', function () {
+    mongodb.MongoClient.connect(url, opt, function (error, client) {
+      if (error)
+        throw error
+      const urlObj = Url.parse(url)
+      const dbName = urlObj.pathname.replace('/', '')
+      F.MongoDB = client.db(dbName)
+      F.emit('database', F.MongoDB)
+      console.log('connected to database ' + dbName)
+      F.wait('mongodb')
+    })
   })
 }
